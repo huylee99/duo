2f9f9f241ce624f20cd2c160a89bb0a74db5dbf2
huylee99 <nhathuyle083@outlook.com>
2023-06-12 23:14:26 -0700

    [fix] env variables missing

--- src/env.mjs
+++ src/env.mjs
@@ -3,15 +3,22 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    KEY: z.string().min(1),
+    DATABASE_HOST: z.string().min(1),
+    DATABASE_USERNAME: z.string().min(1),
+    DATABASE_PASSWORD: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
     NODE_ENV: z.enum(["development", "production", "test"]),
   },
-  client: {
-    NEXT_PUBLIC_KEY: z.string().min(1),
-  },
+  client: {},
   runtimeEnv: {
-    KEY: process.env.KEY,
-    NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+    DATABASE_URL: process.env.DATABASE_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
   },
 });
