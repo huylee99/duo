c4a5378cecd0abe298abfa401b246024ddb1a684
huylee99 <nhathuyle083@outlook.com>
2023-07-11 21:01:51 -0700

    fix: fix type for selected date

--- src/components/dashboard/discounts/discount-form.tsx
+++ src/components/dashboard/discounts/discount-form.tsx
@@ -125,7 +125,7 @@ const DiscountForm = (props: DiscountFormProps) => {
                       </FormControl>
                     </PopoverTrigger>
                     <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date: Date) => date > new Date() || date < new Date("1900-01-01")} initialFocus />
+                      <Calendar mode="single" selected={field.value ? field.value : undefined} onSelect={field.onChange} disabled={(date: Date) => date > new Date() || date < new Date("1900-01-01")} initialFocus />
                     </PopoverContent>
                   </Popover>
                 </FormItem>
@@ -147,7 +147,7 @@ const DiscountForm = (props: DiscountFormProps) => {
                       </FormControl>
                     </PopoverTrigger>
                     <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date: Date) => date > new Date() || date < new Date("1900-01-01")} initialFocus />
+                      <Calendar mode="single" selected={field.value ? field.value : undefined} onSelect={field.onChange} disabled={(date: Date) => date > new Date() || date < new Date("1900-01-01")} initialFocus />
                     </PopoverContent>
                   </Popover>
                 </FormItem>
